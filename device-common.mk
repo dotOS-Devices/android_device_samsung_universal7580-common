459b66f20058d7a9c4a23dbe676065d0cea05f0e
Dario Trombello <dariotr@outlook.de>
2019-08-27 18:27:38 +0200

    Revert "universal7580: Build Trust HAL"
    This reverts commit f7e2172ba051fea1dea5fbd6a1cfa56dc2422019.

--- device-common.mk
+++ device-common.mk
@@ -157,10 +157,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
